7d6d80718210bdce117880d21cb6c16daadf4800
Matvey Kosyakov <matveykos04@gmail.com>
2023-07-18 14:33:23 +0300

    Added auth with FastAPI Users

---
+++ auth/auth.py
@@ -0,0 +1,17 @@
+# Cookie for transporting user's token (For authentication)
+# JWT for storing user's token
+from fastapi_users.authentication import CookieTransport, JWTStrategy, AuthenticationBackend
+from config import SECRET
+
+cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=3600)
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
